e854847cf252e08ee2414c9b85d2ea0aad001453
Inna Pokydko <inna.pokydko@gmail.com>
2023-04-30 10:55:49 +0300

    app done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,140 +8,84 @@ import Button from './Button/Button';
 import Modal from './Modal/Modal';
 import { AppContainer, IdleMessage } from './App.styled';
 import 'react-toastify/dist/ReactToastify.css';
-import { useEffect } from 'react';
 
 const API_KEY = '34168491-a08a19ec58377d1b70d25ff83';
 const PER_PAGE = 12;
 
-export default function App(){
+function App() {
   const [images, setImages] = useState([]);
-const [error, setError] = useState(null);
-const [status, setStatus] = useState('idle');
-const [query, setQuery] = useState('');
-const [page, setPage] = useState(1);
-const [showModal, setShowModal] = useState(false);
-const [imgModal, setImgModal] = useState('');
-const [showLoadMore, setShowLoadMore] = useState(false);
+  const [error, setError] = useState(null);
+  const [status, setStatus] = useState('idle');
+  const [query, setQuery] = useState('');
+  const [page, setPage] = useState(1);
+  const [showModal, setShowModal] = useState(false);
+  const [imgModal, setImgModal] = useState('');
+  const [showLoadMore, setShowLoadMore] = useState(false);
 
-const onFormSubmit = input => {
-  if (input.trim() === '') {
-          return toast.error('Type something!');
-        } else if (input === query) {
-          return;
+  const onFormSubmit = input => {
+    if (input.trim() === '') {
+      return toast.error('Type something!');
+    } else if (input === query) {
+      return;
+    }
+    setQuery(input.toLowerCase());
+    setPage(1);
+    setImages([]);
+  };
+
+  const handleLoadMore = () => {
+    setPage(prevPage => prevPage + 1);
+  };
+
+  const toggleModal = () => {
+    setShowModal(prevShowModal => !prevShowModal);
+  };
+
+  const onShowModal = item => {
+    setImgModal(item);
+    toggleModal();
+  };
+  useEffect(() => {
+    if (query === '') {
+      return;
+    }
+    setStatus('pending');
+    axios
+      .get(
+        `https://pixabay.com/api/?key=${API_KEY}&q=${query}&page=${page}&per_page=${PER_PAGE}`
+      )
+      .then(response => {
+        setImages(prevImages =>
+          page === 1
+            ? response.data.hits
+            : [...prevImages, ...response.data.hits]
+        );
+        setStatus('resolved');
+        setShowLoadMore(response.data.hits.length === PER_PAGE);
+      })
+      .catch(error => {
+        console.log(error);
+        setError(error);
+        setStatus('rejected');
+      });
+  }, [query, page]);
+
+  return (
+    <AppContainer>
+      <Searchbar onSubmit={onFormSubmit} />
+      {status === 'idle' && <IdleMessage>Enter a search query</IdleMessage>}
+      {status === 'pending' && <Loader loading={true} />}
+      {status === 'rejected' && <h2>{error.message}</h2>}
+      {images.length > 0 && (
+        <div>
+          <ImageGallery images={images} onShowModal={onShowModal} />
+          {showLoadMore ? <Button onClick={handleLoadMore} /> : null}
+          <ToastContainer />
+          {showModal && <Modal onClose={toggleModal} item={imgModal} />}
+        </div>
+      )}
+    </AppContainer>
+  );
 }
-setQuery(input.toLowerCase());
-setPage(1);
-setImages([]);
-}
 
-useEffect
-
-return (
-        <AppContainer>
-          <Searchbar onSubmit={onFormSubmit} />
-          {status === 'idle' && <IdleMessage>Enter a search query</IdleMessage>}
-          {status === 'pending' && <Loader loading={true} />}
-          {status === 'rejected' && <h2>{error.message}</h2>}
-          {images.length > 0 && (
-            <div>
-              <ImageGallery images={images} onShowModal={this.onShowModal} />
-              {showLoadMore ? <Button onClick={handleLoadMore} /> : null}
-              <ToastContainer />
-              {showModal && <Modal onClose={toggleModal} item={imgModal} />}
-            </div>
-          )}
-        </AppContainer>
-      );
-}
-
-// class App extends Component {
-//   state = {
-//     images: [],
-//     error: null,
-//     status: 'idle',
-//     query: '',
-//     page: 1,
-//     showModal: false,
-//     imgModal: '',
-//     showLoadMore: false,
-//   };
-
-//   onFormSubmit = input => {
-//     if (input.trim() === '') {
-//       return toast.error('Type something!');
-//     } else if (input === this.state.input) {
-//       return;
-//     }
-//     this.setState({
-//       query: input.toLowerCase(),
-//       page: 1,
-//       images: [],
-//     });
-//   };
-
-//   componentDidUpdate(prevProps, prevState) {
-//     if (
-//       prevState.query !== this.state.query ||
-//       prevState.page !== this.state.page
-//     ) {
-//       this.setState({ status: 'pending' });
-//       axios
-//         .get(
-//           `https://pixabay.com/api/?key=${API_KEY}&q=${this.state.query}&page=${this.state.page}&per_page=${PER_PAGE}`
-//         )
-//         .then(response => {
-//           this.setState(prevState => ({
-//             images:
-//               prevState.page === 1
-//                 ? response.data.hits
-//                 : [...prevState.images, ...response.data.hits],
-//             status: 'resolved',
-//             showLoadMore: response.data.hits.length === PER_PAGE,
-//           }));
-//         })
-//         .catch(error => {
-//           console.log(error);
-//           this.setState({ error, status: 'rejected' });
-//         });
-//     }
-//   }
-
-//   handleLoadMore = () => {
-//     this.setState(prevState => ({ page: prevState.page + 1 }));
-//   };
-
-//   toggleModal = () => {
-//     this.setState(({ showModal }) => ({
-//       showModal: !showModal,
-//     }));
-//   };
-
-//   onShowModal = item => {
-//     this.setState({ imgModal: item, showModal: this.toggleModal });
-//   };
-
-//   render() {
-//     const { images, error, status, showModal, imgModal, showLoadMore } =
-//       this.state;
-
-//     return (
-//       <AppContainer>
-//         <Searchbar onSubmit={this.onFormSubmit} />
-//         {status === 'idle' && <IdleMessage>Enter a search query</IdleMessage>}
-//         {status === 'pending' && <Loader loading={true} />}
-//         {status === 'rejected' && <h2>{error.message}</h2>}
-//         {images.length > 0 && (
-//           <div>
-//             <ImageGallery images={images} onShowModal={this.onShowModal} />
-//             {showLoadMore ? <Button onClick={this.handleLoadMore} /> : null}
-//             <ToastContainer />
-//             {showModal && <Modal onClose={this.toggleModal} item={imgModal} />}
-//           </div>
-//         )}
-//       </AppContainer>
-//     );
-//   }
-// }
-
-// export default App;
-
+export default App;
